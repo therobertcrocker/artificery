631214561a0c5c72879912df01b915bd01233ab1
Robert Crocker <crocker2@wwu.edu>
2022-07-11 08:07:48 -0700

    feat: added local docker support and started code for spellbook.py

--- artificery/database.py
+++ artificery/database.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
-CONNECTION_STRING = "mongodb+srv://ulsidor:hKnFJ78wFMa2Z3MF@grimoire.shjxx.mongodb.net/?retryWrites=true&w=majority"
+CONNECTION_STRING_ATLAS = "mongodb+srv://ulsidor:hKnFJ78wFMa2Z3MF@grimoire.shjxx.mongodb.net/?retryWrites=true&w=majority"
+CONNECTION_STRING_LOCAL = "mongodb://localhost:27017/"
 LOOT_DATA = "loot_data"
 GEMSTONES = "gemstones"
 TRINKETS = "trinkets"
@@ -9,7 +10,7 @@ TRINKETS = "trinkets"
 class Database:
 
     def __init__(self) -> None:
-        self.client = MongoClient(CONNECTION_STRING)
+        self.client = MongoClient(CONNECTION_STRING_LOCAL)
         self.db = self.client[LOOT_DATA]
         self.gemstones = self.db[GEMSTONES]
         self.trinkets = self.db[TRINKETS]
